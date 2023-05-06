0da4a0479262bcc7ef773704c2430a4575c9b249
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-06 13:50:54 +0300

    StreamS
    Writable and readable stream

--- Streams/readWriteStrea.js
+++ Streams/readWriteStrea.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./Streams/data.txt');
 const writeStream = fs.createWriteStream('./Streams/dataCopy.txt');
 
-readStream.on('data', (chunk)=>{
+readStream.on('data', (chunk) => {
     console.log('Write Stream');
     writeStream.write(chunk);
 });
 
-readStream.on('close', ()=>{
+readStream.on('close', () => {
     console.log('Stream Closed');
     writeStream.end();
 });
