c1d1d30434f69aa7e63f92bd275f4b42c726ee99
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-06 13:48:39 +0300

    Streams
    Pipe

--- Streams/pipe.js
+++ Streams/pipe.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./Streams/data.txt');
-const writeStream = fs.createWriteStream('./Streams/dataCopyPipe.txt');
+const writeStream = fs.createWriteStream('./Streams/dataPipeCopy.txt');
 
 readStream.pipe(writeStream);
