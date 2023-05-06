4dac8b57d8eed58b2e1604bcba3895a3cd4152d6
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-06 13:51:19 +0300

    Streams
    Transforming stream

--- Streams/transformStream.js
+++ Streams/transformStream.js
@@ -4,6 +4,6 @@ const zlib = require('zlib');
 const gzip = zlib.createGzip();
 
 const readStream = fs.createReadStream('./Streams/data.txt');
-const writeStream = fs.createWriteStream('./Streams/dataTransformed.txt');
+const writeStream = fs.createWriteStream('./Streams/transformedData.txt');
 
 readStream.pipe(gzip).pipe(writeStream);
