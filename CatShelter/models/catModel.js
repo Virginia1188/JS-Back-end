37f98e60799aa7dbc5f676edf18697f283ad0a20
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-17 14:53:21 +0300

    updated
    Added function for adding new breed and updating the breed on add cat page and edit cat page

--- CatShelter/models/catModel.js
+++ CatShelter/models/catModel.js
@@ -1,5 +1,6 @@
 const cats = require('../cats.json');
 const utils = require('../utils');
+const breeds = require('../breed.json');
 
 function findAll(){
     return new Promise((resolve,reject)=>{
@@ -17,10 +18,7 @@ function findById(id){
 
 function create(cat){
     return new Promise((resolve,reject)=>{
-        // const cat = cats.find((c) => c.id == id);
-        // console.log(cat);
-        // resolve(cat);
-
+        
         const newCat = {id: createId() , ...cat};
         cats.push(newCat);
         utils.writeDataToFile('./cats.json', cats);
@@ -28,6 +26,30 @@ function create(cat){
     });
 }
 
+async function deleteCat(filename,catId){
+    
+    new Promise((resolve,reject)=>{
+        const updated =  utils.deleteData(filename, catId);
+        return resolve(updated);
+    });
+
+}
+
+async function breed(newBreed){
+    return new Promise((resolve,reject)=>{
+        breeds.push(newBreed);
+        utils.writeDataToFile('./breed.json',breeds);
+        resolve(newBreed);
+    });
+    
+}
+
+async function updateBreed(html){
+    const breedHtml = await utils.readFile('./views/partials/breed.html');
+    const breedsTemp = breeds.map(breed => breedHtml.replaceAll('{{breedName}}',breed.breedName));
+    const result = html.replace('{breed}',breedsTemp);
+    return result;
+}
 function createId (){
     return cats.length + 1;
 }
@@ -36,4 +58,7 @@ module.exports = {
     findAll,
     findById,
     create,
+    deleteCat,
+    breed,
+    updateBreed,
 };
