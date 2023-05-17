d82d31d103981159520f4538388100b0037741a8
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-17 14:52:02 +0300

    Updated
    Added route for add and delete cat, as well for adding new breed

--- CatShelter/index.js
+++ CatShelter/index.js
@@ -1,19 +1,20 @@
 const http = require('http');
 const cats = require('./cats.json');
 const path = require('path');
+const url = require('url');
 
-const {getCats, getCat, createCat} = require('./controllers/catController');
-const {readFile, catTemplate} = require('./utils');
+const { getCats, getCat, createCat, addBreed } = require('./controllers/catController');
+const { readFile, deleteData } = require('./utils');
+const { deleteCat, updateBreed } = require('./models/catModel');
+const { error } = require('console');
 
 
 const server = http.createServer(async (req, res) => {
 
-    // console.log(req.url);
-
     if (req.url == '/' && req.method === 'GET') {
-  
-        await getCats(req,res);
-      
+
+        await getCats(req, res);
+
     } else if (req.url.includes('site.css')) {
 
         res.writeHead(200, {
@@ -23,12 +24,15 @@ const server = http.createServer(async (req, res) => {
         res.write(siteCss);
     } else if (/cats\/\d+\/edit/.test(req.url) && req.method === 'GET') {
         let catId = req.url.split('/')[2];
-        await getCat(req,res,catId);
+        await getCat(req, res, catId);
         // let catId = req.url.split('/')[2];
         // let cat = cats.find(x => x.id == catId);
 
-    }else if(req.url == '/add-cat/' && req.method === 'GET'){
-        createCat(req,res);
+    } else if (/add-cat\?\w+/.test(req.url) && req.method === 'GET') {
+
+        await createCat(req, res);
+        res.writeHead(301, { 'Location': '/' });
+        res.end();
     } else if (/cats\/\d+\/shelter/.test(req.url)) {
 
         let catId = req.url.split('/')[2];
@@ -36,13 +40,26 @@ const server = http.createServer(async (req, res) => {
 
         const shelterCat = await readFile('./views/catShelter.html');
         res.write(shelterCat);
-    } else if(req.url == '/add-breed'){
+    } else if (req.url == '/add-breed') {
         const addBreed = await readFile('./views/addBreed.html');
         res.write(addBreed);
-    } else if(req.url == '/add-cat'){
+    } else if (/add-breed\?\w+/.test(req.url) && req.method === 'GET') {
+
+        await addBreed(req,res);
+        res.writeHead(301, { 'Location': '/' });
+
+    }else if (req.url == '/add-cat' && req.method == 'GET') {
         const addCat = await readFile('./views/addCat.html');
-        res.write(addCat);
-    }else {
+        const result = await updateBreed(addCat);
+        res.write(result);
+    } else if (/delete/.test(req.url) && req.method == 'GET') { // need to make changes appear when redirected 
+        let catId = req.url.split('/')[2];
+       
+        const promise = await deleteCat('./cats.json', catId);
+        res.writeHead(301, { 'Location': '/' });
+        console.log(error);
+
+    } else {
 
         res.write('<h1>404</h1>');
     }
