eacaf94054192d655c5ac31213a994a16906a93c
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-26 22:47:25 +0300

    Setting express and handlebars

--- Cubicle/src/index.js
+++ Cubicle/src/index.js
@@ -1,1 +1,20 @@
-console.log('hi');
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+
+const app = express();
+const PORT = 5000;
+
+app.engine('hbs',handlebars.engine({
+    extname:'hbs',
+
+}));
+app.set('view engine','hbs');
+app.set('views','src/views');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+
+app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}...`));
