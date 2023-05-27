5192fbb790c1e19193009f64e1f075dad1c3f3ed
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-27 16:43:20 +0300

    Adding handlebars configurator

--- Cubicle/src/index.js
+++ Cubicle/src/index.js
@@ -1,22 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
 const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
 
 
 const app = express();
 const PORT = 5000;
 
 expressConfig(app);
+handlebarsConfig(app);
 
-app.engine('hbs',handlebars.engine({
-    extname:'hbs',
-
-}));
-app.set('view engine','hbs');
-app.set('views','src/views');
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
