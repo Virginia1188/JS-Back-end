1913b34c41aa730a02afa0cc026a904bf3dee3b6
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-28 11:14:12 +0300

    Add search logic

--- Cubicle/src/managers/cubeManager.js
+++ Cubicle/src/managers/cubeManager.js
@@ -8,8 +8,19 @@ const cubes = [
     }
 ];
 const uniqId = require('uniqid');
-exports.getAll = () => cubes.slice();
-
+exports.getAll = (search,from,to) => {
+    let result = cubes.slice();
+    if(search){
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+    if(from){
+        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+    }
+    if(to){
+        result = result.filter(cube => cube.difficultyLevel <= Number(to));
+    }
+    return result;
+};
 exports.create = (cubeData) => {
     const newCube = {
         id: uniqId(),
