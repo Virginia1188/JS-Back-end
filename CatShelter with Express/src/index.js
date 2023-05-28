ba04e1e179b2c8801957d847b06864b0802d4143
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-28 22:36:56 +0300

    Add cat controller

--- CatShelter with Express/src/index.js
+++ CatShelter with Express/src/index.js
@@ -4,6 +4,7 @@ const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 
 const homeController = require('./controllers/homeController');
+const catController = require('./controllers/catController');
 
 const app = express();
 const port = 5000;
@@ -12,6 +13,7 @@ expressConfig(app);
 handlebarsConfig(app);
 
 app.use(homeController);
+app.use('/cats',catController);
 
 
 
