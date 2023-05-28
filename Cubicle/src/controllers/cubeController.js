9376511e0b439a5eaebb95d149564b293f651ba9
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-28 11:01:38 +0300

    Add 404 page

--- Cubicle/src/controllers/cubeController.js
+++ Cubicle/src/controllers/cubeController.js
@@ -7,20 +7,24 @@ router.get('/create', (req, res) => {
 });
 
 router.post('/create', (req, res) => {
-    const { name, description, imageUrl,difficultyLevel } = req.body;
+    const { name, description, imageUrl, difficultyLevel } = req.body;
     //add validation
-    cubeManager.create({ 
-        name, 
-        description, 
-        imageUrl, 
-        difficultyLevel:Number(difficultyLevel),
+    cubeManager.create({
+        name,
+        description,
+        imageUrl,
+        difficultyLevel: Number(difficultyLevel),
     });
     res.redirect('/');
 });
 
-router.get('/:cubeId/details',(req,res)=>{
+router.get('/:cubeId/details', (req, res) => {
+
     const cube = cubeManager.getOne(req.params.cubeId);
-    res.render('details',{...cube});
+    if(!cube){
+        return res.redirect('/404');
+    }
+    res.render('details', { ...cube });
 });
 
 module.exports = router;
