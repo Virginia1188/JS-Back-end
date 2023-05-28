012c3a8fa27203fcbdab7034679294d38ca73554
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-28 10:51:46 +0300

    Add cube details page

--- Cubicle/src/managers/cubeManager.js
+++ Cubicle/src/managers/cubeManager.js
@@ -1,15 +1,25 @@
-const cubes = [];
+const cubes = [
+    {
+        id: '1ugewj22p8li6f6e35',
+        name: 'Eco-Dark',
+        description: 'Something, something',
+        imageUrl: 'https://thingsidesire.com/wp-content/uploads/2018/06/Eco-Dark-Rubik%E2%80%99s-Cube2.jpg',
+        difficultyLevel: 4
+    }
+];
 const uniqId = require('uniqid');
-exports.getAll = ()=> cubes.slice();
+exports.getAll = () => cubes.slice();
 
-exports.create = (cubeData)=> {
+exports.create = (cubeData) => {
     const newCube = {
         id: uniqId(),
         ...cubeData,
     };
-    
+
     cubes.push(newCube);
     return newCube;
 
 };
+
+exports.getOne = (cubeId) => cubes.find(x=>x.id == cubeId);
 
