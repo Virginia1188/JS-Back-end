d4f83f9d4cd5ac23b2535da7fd2cf051bc90495f
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-28 11:23:27 +0300

    Extract routes

--- Cubicle/src/index.js
+++ Cubicle/src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 5000;
@@ -10,11 +9,6 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController);
-app.use('/cubes',cubeController);
-app.get('*', (req,res)=>{
-    res.redirect('/404');
-});
-
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
