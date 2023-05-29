2f7de1ea3a9f0484d68659351dc50145749b7b2f
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-29 12:57:52 +0300

    add a cat functionality

--- CatShelter with Express/src/controllers/catController.js
+++ CatShelter with Express/src/controllers/catController.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const Cat = require('../models/Cat');
 
 router.get('/add-cat', (req,res)=>{
     res.render('addCat');
+});
+
+router.post('/add-cat', async (req,res)=>{
+    const {name,description, image, breed} = req.body;
+    await Cat.addCat({name,description, image, breed});
+    res.redirect('/');
 });
 
 router.get('/delete:catId', (req,res)=>{
