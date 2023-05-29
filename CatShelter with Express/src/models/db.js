0f1b7497ba399fd21ace744c00cb0426151f04bb
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-29 12:58:07 +0300

    add a cat functionality

--- CatShelter with Express/src/models/db.js
+++ CatShelter with Express/src/models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB(){
     try {
-        mongoose.set('stringQuery', false);
+        mongoose.set('strictQuery', false);
         await mongoose.connect('mongodb://127.0.0.1:27017/catShelter');
         console.log('DB Connected');
     } catch (error) {
