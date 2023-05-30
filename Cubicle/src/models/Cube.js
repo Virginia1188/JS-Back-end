829d6d1e28b5cbe9fa99f087858e27cc239bd758
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-30 10:43:36 +0300

    Add base cube model

---
+++ Cubicle/src/models/Cube.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    imageUrl: String,
+    difficultyLevel: Number,
+
+});
+
+const Cube = mongoose.model('Cube', cubeSchema);
+
+module.exports = Cube;
