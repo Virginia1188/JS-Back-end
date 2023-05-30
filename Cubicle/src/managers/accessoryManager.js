3b5c5f626efdc8a753c8bc0304abe48118a4a018
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-05-30 15:37:54 +0300

    Show remaining accessories only

--- Cubicle/src/managers/accessoryManager.js
+++ Cubicle/src/managers/accessoryManager.js
@@ -3,3 +3,5 @@ const Accessesory = require('../models/Accessory');
 exports.create = (accessoryData) => Accessesory.create(accessoryData);
 
 exports.getAll = ()=> Accessesory.find();
+
+exports.getAvailable = (accessoryIds) =>Accessesory.find({_id: {$nin: accessoryIds}});
