02976f8076d7c314ca1f46abf9c5c45689ce4c48
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-06-02 12:16:33 +0300

    Add validation for register data

--- Cubicle/src/controllers/authController.js
+++ Cubicle/src/controllers/authController.js
@@ -1,15 +1,33 @@
 const router = require('express').Router();
+const authManager = require('../managers/authManager');
 
-router.get('/login', (req,res)=>{
+router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.get('/register', (req,res)=>{
+router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.get('/logout', (req,res)=>{
+router.get('/logout', (req, res) => {
     res.render('auth/logout');
+});
+
+router.post('auth/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    if (password !== repeatPassword) {
+        return res.status(404).end();
+    }
+
+    const existingUser = await authManager.getUseByUsername(username);
+
+    if (existingUser) {
+        return res.status(404).end();
+    }
+
+    const user = await authManager.register(username, password);
+
 });
 
 module.exports = router;
