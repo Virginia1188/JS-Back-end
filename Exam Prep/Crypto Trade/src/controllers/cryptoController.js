73c82ad17eaa25f2ee873a1c449e83be7db1a2bf
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-06-13 14:31:04 +0300

    Error handling for create page

--- Exam Prep/Crypto Trade/src/controllers/cryptoController.js
+++ Exam Prep/Crypto Trade/src/controllers/cryptoController.js
@@ -1,18 +1,26 @@
 const router = require('express').Router();
+const cryptoManager = require('../managers/cryptoManager');
+const {getErrorMessage} = require('../utils/errorUtils');
 
-router.get('/create',(req,res)=>{
+router.get('/create', (req, res) => {
     res.render('crypto/create');
 });
 
-router.post('/create',async (req,res)=>{
-    const {name,image,price,description, payment} = req.body;
+router.post('/create', async (req, res) => {
+    const { name, image, price, description, payment } = req.body;
     const owner = req.user._id;
-    console.log(owner);
 
-    await Crypto.create(name,image,price,description, payment, owner);
+    try {
+        await cryptoManager.create(name, image, price, description, payment, owner);
+        res.redirect('/crypto/catalog');
+
+    } catch (error) {
+        return res.status(404).render('crypto/create', { error: getErrorMessage(error) });
+    }
+
 });
 
-router.get('/catalog', (req,res)=>{
+router.get('/catalog', (req, res) => {
     res.render('crypto/catalog');
 });
 
