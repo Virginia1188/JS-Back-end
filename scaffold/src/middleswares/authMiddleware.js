afe873162f8711cf63db31d1e7400cdeba84ab44
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-06-13 13:22:52 +0300

    Scaffold for exam

---
+++ scaffold/src/middleswares/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require('../lib/jsonwebtoken');
+const { SECRET } = require('../config/dbConfig');
+
+
+exports.authentication = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token, SECRET);
+
+            req.user = decodedToken;
+            res.locals.isAuthenticated = true;
+            res.locals.user = decodedToken;
+
+            next();
+
+        } catch (error) {
+            res.clearCookie('auth');
+            return res.status(401).render('/404');
+        }
+
+    }else{
+        next();
+    }
+};
+
+exports.isAuth = (req, res, next) => {
+    if (!req.user) {
+        res.redirect('/users/login');
+    }
+    next();
+};
